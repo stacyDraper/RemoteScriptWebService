89d7c8a262a8de8ebb0e12a68e0ad86a7a6e3cc4
stacyDraper <stacyDraper@wildwires.com>
2016-08-06 12:35:13 -0700

    happy path works.  The modal waits for the asp.net code to finish.  may change it to be async or put it all in javascript.

---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Execute execute = new Execute();
+        string jobSiteUrl = Request.QueryString["jobSiteUrl"];
+        string retValue = "";
+
+        string id = Request.QueryString["id"];
+        if (id.Contains("|"))
+        {
+            string[] ids = id.Split('|');
+            foreach (string identity in ids)
+            {
+                if (!string.IsNullOrEmpty(identity))
+                {
+                    execute.Job(identity, jobSiteUrl);
+                }
+            }
+        }
+        else
+        {
+            retValue = execute.Job(id, jobSiteUrl);
+        }
+        
+        Response.Write("The dishes are done, dude!");
+    }
+}
